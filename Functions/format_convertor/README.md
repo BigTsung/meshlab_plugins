1ef26d75cefa232ff3b6065f4d17264d7420f436
BigTsung <yutsungchiu@gmail.com>
2022-05-03 23:13:38 +0800

    created a new function format convertor

---
+++ Functions/format_convertor/README.md
@@ -0,0 +1,6 @@
+
+## How to use?
+```
+$ pip3 install -r ./requirements.txt
+$ python3 format_convertor.py -i ~/Desktop/model.ply -o ~/Desktop -f ply
+```
---
+++ Functions/format_convertor/format_convertor.py
@@ -0,0 +1,27 @@
+import pymeshlab
+import argparse
+import glob, os
+from pathlib import Path
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "-input_path", type=str, help="directory of model")
+    parser.add_argument("-f", "-format", type=str, help="the target format.(ex: .obj .ply .xyz .e57 .off .stl)")
+    parser.add_argument("-o", "-output_path", type=str, help="output path")
+    args = parser.parse_args()
+    inputPath = args.i
+    format = args.f
+    outputPath = args.o
+    print("Input path: ", inputPath)
+    ms = pymeshlab.MeshSet()
+
+    ms.load_new_mesh(inputPath)
+    
+    fileName = Path(inputPath).stem
+
+    finalOutput = os.path.join(outputPath, fileName + "." + format)
+
+    print("Export: ", finalOutput)
+    ms.save_current_mesh(file_name=finalOutput)		
+    # ms.save_current_mesh(outputPath)
+    print("Converted!")
